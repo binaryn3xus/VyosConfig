a3388744137eef81438277bbedc6ce70b8490f3d
joshuagarrison27 <joshuagarrison27@gmail.com>
2023-08-27 21:11:38 -0400

    Changes for firewall command change

--- config-parts/firewall.sh
+++ config-parts/firewall.sh
@@ -1,14 +1,23 @@
 #!/bin/vbash
 
-# General configuration
-set firewall state-policy established action 'accept'
-set firewall state-policy invalid action 'drop'
-set firewall state-policy related action 'accept'
-set firewall all-ping 'enable'
+# Global Options
+
+set firewall global-options all-ping 'enable'
 
 # Address Groups
 
+set firewall group interface-group IG_containers interface 'cni-containers'
+set firewall group interface-group IG_containers interface 'pod-containers'
+set firewall group interface-group IG_guest interface 'eth1.40'
+set firewall group interface-group IG_lan interface 'eth1'
+set firewall group interface-group IG_servers interface 'eth1.30'
+set firewall group interface-group IG_trusted interface 'eth1.20'
+set firewall group interface-group IG_wan interface 'eth0'
+set firewall group interface-group IG_wireguard interface 'wg50'
+
 set firewall group address-group k8s-ingress address '10.0.30.32'
+set firewall group network-group k8s-ingress description 'Kubernetes IPv4 Host'
+
 set firewall group network-group cloudflare-networks-v4 description 'Task Managed: Cloudflare IPv4 Hosts'
 set firewall group network-group cloudflare-networks-v4 network '173.245.48.0/20'
 set firewall group network-group cloudflare-networks-v4 network '103.21.244.0/22'
